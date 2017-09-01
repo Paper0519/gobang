58759a74167ae59b3c70c188d87c7a0c03795cd3
苟培博 <goupeibo@rippletek.com>
2017-09-01 14:53:59 +0800

    user styled-component for single component

--- src/chessboard/index.js
+++ src/chessboard/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import { BOARD_SIZE } from '../app/constants';
-import './index.css';
+import styled from 'styled-components';
+
+const Ul = styled.ul`
+    position: absolute;
+    top: 25px;
+    left: 25px;
+    width: 700px;
+    height: 700px;
+    border-top: 1px solid #000;
+    background-color: #f5ad45;
+    z-index: 1;
+`;
+
+const Li = styled.li`
+    border: 1px solid #000;
+    border-width: 0 1px 1px 0;
+    &:nth-child(14n-13) {
+        border-left-width: 1px;
+    }
+`;
 
 const Chessboard = () => {
     const [x, y] = BOARD_SIZE;
@@ -8,12 +27,12 @@ const Chessboard = () => {
     for (let i = 0; i < x - 1; i++) {
         for (let j = 0; j < y - 1; j++) {
             chess.push(
-                <li
+                <Li
                     key={`square-${i}-${j}`} />
             )
         }
     }
-    return <ul className="real-chessboard">{ chess }</ul>;
+    return <Ul>{ chess }</Ul>;
 };
 
 export default Chessboard;
